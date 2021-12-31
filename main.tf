2396739fcb39fc8457ef684f63c7c921072766f2
Diya Moliki <diyamoliki@yahoo.com>
2021-12-30 21:19:59 -0500

    remote-backend

--- main.tf
+++ main.tf
@@ -1,5 +1,31 @@
-resource "aws_instance" "web_server" {
-    ami = "ami-04902260ca3d33422"
-    instance_type = "t2.micro" 
-    count = 2
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+  bucket = "terraform-forolajidecaleb"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-forolajide"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
